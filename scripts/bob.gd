7e4a9be5f94cf0575b457d2480db5188e8c7329c
Programmer Cheema <47918386+bilalCh213@users.noreply.github.com>
2025-01-25 16:55:56 +0500

    Improved platformer and removed some assets.

--- scripts/bob.gd
+++ scripts/bob.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+@export var transfer_position : Node2D
+
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -720.0
+const MAX_VELOCITY = 800
 
 func _physics_process(delta: float) -> void:
 	
-	if not is_on_floor():
+	if not is_on_floor() and velocity.length() < MAX_VELOCITY:
 		velocity += get_gravity() * delta
 
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -18,3 +21,8 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_bottom_fall_detector_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		body.global_position.y = transfer_position.global_position.y
